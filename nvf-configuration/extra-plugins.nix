f10be289bd18194d56c6defc66f23c420e0bdc3c
alexinatra2 <a.holzknecht@gmx.de>
2025-01-07 15:43:51 +0100

    add harpoon mappings

--- nvf-configuration/extra-plugins.nix
+++ nvf-configuration/extra-plugins.nix
@@ -1,16 +1,53 @@
 { pkgs, ... }:
 {
-  programs.nvf.settings.vim.extraPlugins = with pkgs.vimPlugins; {
-    aerial = {
-      package = aerial-nvim;
-      setup = ''
-        require('aerial').setup {}
-      '';
-    };
+  programs.nvf.settings.vim.lazy.plugins = with pkgs.vimPlugins; {
     harpoon = {
       package = harpoon;
-      setup = "require('harpoon').setup {}";
-      after = [ "aerial" ];
+      event = [ "BufEnter" ];
+      keys = [
+        {
+          key = "<leader>a";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.mark').add_file()<cr>";
+          desc = "Add harpoon file";
+        }
+        {
+          key = "<leader>s";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.ui').toggle_quick_menu()<cr>";
+          desc = "Show harpoon files";
+        }
+        {
+          key = "<leader>1";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.ui').nav_file(1)<cr>";
+          desc = "Navigate to harpoon file 1";
+        }
+        {
+          key = "<leader>2";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.ui').nav_file(2)<cr>";
+          desc = "Navigate to harpoon file 2";
+        }
+        {
+          key = "<leader>3";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.ui').nav_file(3)<cr>";
+          desc = "Navigate to harpoon file 3";
+        }
+        {
+          key = "<leader>4";
+          mode = "n";
+          silent = true;
+          action = ":lua require('harpoon.ui').nav_file(4)<cr>";
+          desc = "Navigate to harpoon file 4";
+        }
+      ];
     };
   };
 }
