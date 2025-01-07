a7c6b18bed8ced91ee9a2d3eb3f757180ebbcf65
alexinatra2 <a.holzknecht@gmx.de>
2025-01-07 14:41:19 +0100

    enable visuals

--- nvf-configuration/ui.nix
+++ nvf-configuration/ui.nix
@@ -1,9 +1,16 @@
 { ... }:
 {
-  programs.nvf.settings.vim.ui = {
-    fastaction.enable = true;
-    illuminate.enable = true;
-    colorizer.enable = true;
-    smartcolumn.enable = true;
+  programs.nvf.settings.vim = {
+    ui = {
+      fastaction.enable = true;
+      illuminate.enable = true;
+      colorizer.enable = true;
+      smartcolumn.enable = true;
+    };
+    visuals = {
+      cinnamon-nvim.enable = true;
+      highlight-undo.enable = true;
+      indent-blankline.enable = true;
+    };
   };
 }
