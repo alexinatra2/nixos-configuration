b1db35a524f1a068e6d1d91d3fa80c43521a4db1
alexinatra2 <a.holzknecht@gmx.de>
2025-01-08 00:46:22 +0100

    fix tmux navigator

--- nvf-configuration/extra-plugins/tmux-nvim.nix
+++ nvf-configuration/extra-plugins/tmux-nvim.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  programs.nvf.settings.vim.lazy.plugins."tmux-nvim" = {
+  programs.nvf.settings.vim.lazy.plugins."tmux.nvim" = {
     package = pkgs.vimPlugins.tmux-nvim;
     event = [ "BufEnter" ];
   };
