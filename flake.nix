b67f36251db29946778dbded591860757150c2d4
alexinatra2 <a.holzknecht@gmx.de>
2025-01-07 10:21:23 +0100

    add nvf setup

--- flake.nix
+++ flake.nix
@@ -29,6 +29,24 @@
     hyprland = {
       url = "github:hyprwm/Hyprland";
     };
+
+    # Optional, if you intend to follow nvf's obsidian-nvim input
+    # you must also add it as a flake input.
+    obsidian-nvim = {
+      url = "github:epwalsh/obsidian.nvim";
+    };
+
+    # Required, nvf works best and only directly supports flakes
+    nvf = {
+      url = "github:notashelf/nvf";
+      # You can override the input nixpkgs to follow your system's
+      # instance of nixpkgs. This is safe to do as nvf does not depend
+      # on a binary cache.
+      inputs.nixpkgs.follows = "nixpkgs";
+      # Optionally, you can also override individual plugins
+      # for example:
+      inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
+    };
   };
 
   outputs =
@@ -36,7 +54,7 @@
       nixpkgs,
       home-manager,
       nixvim,
-      hyprland,
+      nvf,
       ...
     }@inputs:
     let
@@ -47,7 +65,7 @@
     in
     {
       nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
+        "${hostname}" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
@@ -57,11 +75,13 @@
           modules = [
             ./configuration.nix
             inputs.stylix.nixosModules.stylix
+            nvf.nixosModules.default
           ];
         };
       };
+
       homeConfigurations = {
-        alexander = home-manager.lib.homeManagerConfiguration {
+        "${username}" = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           extraSpecialArgs = {
             inherit username;
@@ -69,6 +89,7 @@
             inherit nixvim;
           };
           modules = [
+            nvf.homeManagerModules.default
             ./home.nix
           ];
         };
