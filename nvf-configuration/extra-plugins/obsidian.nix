90e8e546a6e4f437399a9297e426ca885b099a02
alexinatra2 <a.holzknecht@gmx.de>
2025-01-08 23:41:35 +0100

    refactor obsidian plugin definition

--- nvf-configuration/extra-plugins/obsidian.nix
+++ nvf-configuration/extra-plugins/obsidian.nix
@@ -1,16 +1,31 @@
 { pkgs, ... }:
 {
-  programs.nvf.settings.vim.extraPlugins."obsidian.nvim" = {
+  programs.nvf.settings.vim.lazy.plugins."obsidian.nvim" = {
     package = pkgs.vimPlugins.obsidian-nvim;
-    setup = ''
-      require('obsidian').setup {
-        workspaces = {
-          {
-            name = "personal",
-            path = "~/opal-overhaul",
-          },
-        },
+    lazy = true;
+    setupModule = "obsidian";
+    ft = "markdown";
+    setupOpts = {
+      workspaces = [
+        {
+          name = "personal";
+          path = "~/opal-overhaul";
+        }
+      ];
+      completion = {
+        nvim_cmp = true;
+        min_chars = 2;
+      };
+    };
+    keys = [
+      {
+        key = "<cr>";
+        mode = "n";
+        silent = true;
+        lua = true;
+        action = "function() require('obsidian').util.smart_action() end";
+        desc = "Perform Obsidian smart action";
       }
-    '';
+    ];
   };
 }
