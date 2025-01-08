e8a74e0fc7d70f43a1eeb1282a9238c783be3efe
alexinatra2 <a.holzknecht@gmx.de>
2025-01-08 17:26:10 +0100

    add flake = false to obsidian.nvim input

--- flake.nix
+++ flake.nix
@@ -35,7 +35,10 @@
 
     # Optional, if you intend to follow nvf's obsidian-nvim input
     # you must also add it as a flake input.
-    obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
+    obsidian-nvim = {
+      url = "github:epwalsh/obsidian.nvim";
+      flake = false;
+    };
 
     # Required, nvf works best and only directly supports flakes
     nvf = {
