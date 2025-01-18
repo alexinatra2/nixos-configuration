3e5a754b03f2e7ff3107ff58e12b749336c07532
alexinatra2 <a.holzknecht@gmx.de>
2025-01-18 22:10:56 +0100

    refactor niri binds config using spawn function

--- niri.nix
+++ niri.nix
@@ -1,25 +1,38 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
   programs.niri = {
     enable = true;
     settings = {
-      binds = {
-        "Mod+Return".action.spawn = "kitty";
-        "Mod+Shift+F".action.spawn = "firefox";
+      binds =
+        with config.lib.niri.actions;
+        let
+          sh = spawn "sh" "-c";
+        in
+        {
+          "Mod+Return".action = spawn "kitty";
+          "Mod+Shift+F".action = spawn "firefox";
 
-        "Mod+h" = "focus-column-left";
-        "Mod+j".action.focus-window-or-worspace-down = true;
-        "Mod+k".action.focus-window-or-worspace-up = true;
-        "Mod+l" = "focus-column-right";
+          "Mod+h" = "focus-column-left";
+          "Mod+l" = "focus-column-right";
 
-        "Mod+Shift+h".action.set-column-width = "-10%";
-        "Mod+Shift+l".action.set-column-width = "+10%";
+          "Mod+Shift+h".action.set-column-width = "-10%";
+          "Mod+Shift+l".action.set-column-width = "+10%";
 
-        "Mod+Shift+Q".action.quit.skip-confirmation = true;
-      };
+          "XF86AudioRaiseVolume".action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+";
+          "XF86AudioLowerVolume".action = sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
+          "XF86AudioMute".action = sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+
+          "XF86MonBrightnessUp".action = sh "brightnessctl set 10%+";
+          "XF86MonBrightnessDown".action = sh "brightnessctl set 10%-";
+
+          "Mod+Q".action = close-window;
+
+          "Mod+Shift+Q".action.quit.skip-confirmation = true;
+        };
     };
   };
 }
