9a0c8cc839d00836b0c779072f71468af601e73c
alexinatra2 <a.holzknecht@gmx.de>
2025-01-31 10:44:33 +0100

    remove tat tmux alias

--- home/shell.nix
+++ home/shell.nix
@@ -25,20 +25,6 @@ in
         enable = true;
         enableCompletion = true;
         inherit shellAliases;
-        initExtra = ''
-          # tat: tmux attach
-          function tat {
-            name=$(basename `pwd` | sed -e 's/\.//g')
-
-            if tmux ls 2>&1 | grep "$name"; then
-              tmux attach -t "$name"
-            elif [ -f .envrc ]; then
-              direnv exec / tmux new-session -s "$name"
-            else
-              tmux new-session -s "$name"
-            fi
-          }
-        '';
       };
 
       nushell = mkIf cfg.enableNu {
