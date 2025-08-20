dffbb61161d56ae79c502b5a83f6aa99d11994ca
alexinatra2 <a.holzknecht@gmx.de>
2025-08-20 23:09:32 +0200

    change back to default cursor

--- home/stylix.nix
+++ home/stylix.nix
@@ -9,11 +9,6 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     image = ../background.png;
     polarity = "dark";
-    cursor = {
-      name = "phinger-cursors-light";
-      package = pkgs.phinger-cursors;
-      size = 32;
-    };
     targets = {
       firefox.enable = false;
     };
