a9cb64be7362ed30b256dd299c27d1e349d24c00
alexinatra2 <a.holzknecht@gmx.de>
2025-10-21 16:37:36 +0200

    add separate nixpkgs-unstable input to flake and use 25.05 by default

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # home-manager, used for managing user configuration
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       # The `follows` keyword in inputs is used for inheritance.
