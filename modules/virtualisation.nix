22fd09ddb3dad2f5df01bb50151b4e736fbc7c11
alexinatra2 <a.holzknecht@gmx.de>
2025-12-19 13:21:47 +0100

    fix virtualisation

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -15,10 +15,7 @@
     #   # Required for containers under podman-compose to be able to talk to each other.
     #   defaultNetwork.settings.dns_enabled = true;
     # };
-    docker = {
-      enable = true;
-      package = pkgs.docker_25;
-    };
+    docker.enable = true;
   };
 
   # Useful container development tools
