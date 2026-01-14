88f63861a9840a7d74b8e75dc9861010cce3ba38
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-01-14 13:12:05 +0100

    add more telescope keymaps

--- modules/nvf/telescope.nix
+++ modules/nvf/telescope.nix
@@ -13,13 +13,17 @@
 
     keymaps = [
       {
-        mode = [
-          "n"
-          "i"
-          "x"
-        ];
+        mode = "n";
         key = "<leader>fo";
-        action = "<ESC>Telescope oldfiles<CR>";
+        action = "<CMD>Telescope oldfiles<CR>";
+        desc = "Old files [Telescope]";
+        silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>fh";
+        action = "<CMD>Telescope command_history<CR>";
+        desc = "Command history [Telescope]";
         silent = true;
       }
     ];
