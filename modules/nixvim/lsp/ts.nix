fb00349ec2070afdcfc0ef133432382a37382d43
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-01-26 12:57:31 +0100

    configure ts_ls and eslint language servers

--- modules/nixvim/lsp/ts.nix
+++ modules/nixvim/lsp/ts.nix
@@ -1,5 +1,62 @@
 {
-  lsp.servers.ts_ls = {
-    enable = true;
+  lib,
+  pkgs,
+  ...
+}:
+{
+  lsp.servers = {
+    ts_ls = {
+      enable = true;
+
+      config = {
+        cmd = [
+          "${pkgs.nodePackages_latest.typescript-language-server}/bin/typescript-language-server"
+          "--stdio"
+        ];
+
+        typescript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all";
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false;
+            includeInlayFunctionParameterTypeHints = true;
+            includeInlayVariableTypeHints = true;
+            includeInlayPropertyDeclarationTypeHints = true;
+            includeInlayFunctionLikeReturnTypeHints = true;
+            includeInlayEnumMemberValueHints = true;
+          };
+        };
+
+        javascript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all";
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false;
+            includeInlayFunctionParameterTypeHints = true;
+            includeInlayVariableTypeHints = true;
+            includeInlayPropertyDeclarationTypeHints = true;
+            includeInlayFunctionLikeReturnTypeHints = true;
+            includeInlayEnumMemberValueHints = true;
+          };
+        };
+
+        on_attach = lib.nixvim.mkRaw ''
+          function(client, bufnr)
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          end
+        '';
+      };
+    };
+
+    eslint = {
+      enable = true;
+      packageFallback = true;
+      config = {
+        settings = {
+          workingDirectory = {
+            mode = "auto";
+          };
+          nodePath = "node_modules";
+        };
+      };
+    };
   };
 }
