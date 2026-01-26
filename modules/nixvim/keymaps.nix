0890fbc110daaf61490fd7bb67218547c944ebd5
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-01-26 16:10:11 +0100

    add <C-BS> mapping

--- modules/nixvim/keymaps.nix
+++ modules/nixvim/keymaps.nix
@@ -1,8 +1,16 @@
 {
   keymaps = [
     {
-      action = "<cmd>update<CR>";
+      action = "<CMD>update<CR>";
       key = "<C-s>";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      mode = [ "i" ];
+      action = "<C-w>";
+      key = "<A-BS>";
       options = {
         silent = true;
       };
