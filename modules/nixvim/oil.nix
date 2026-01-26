b56da75fa22cc3442d68e3393eeca9a79d26dca1
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-01-26 10:50:26 +0100

    add oil.nix

---
+++ modules/nixvim/oil.nix
@@ -0,0 +1,13 @@
+{
+  plugins.oil.enable = true;
+
+  keymaps = [
+    {
+      action = "<cmd>Oil<CR>";
+      key = "<leader>e";
+      options = {
+        silent = true;
+      };
+    }
+  ];
+}
