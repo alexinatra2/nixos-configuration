8b821a6412a4abb9e71bbbcc1e9fd8311fc1f7c5
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-01-27 11:15:43 +0100

    enable jumping to next/prev hunk

--- modules/nixvim/git.nix
+++ modules/nixvim/git.nix
@@ -3,10 +3,27 @@
   plugins.gitsigns.enable = true;
   keymaps = [
     {
-      action = "<cmd>LazyGit<CR>";
+      action = "<CMD>LazyGit<CR>";
       key = "<leader>gg";
       options = {
         silent = true;
+        desc = "LazyGit";
+      };
+    }
+    {
+      action = "<CMD> Gitsigns next_hunk<CR>";
+      key = "]h";
+      options = {
+        silent = true;
+        desc = "Next git hunk";
+      };
+    }
+    {
+      action = "<CMD> Gitsigns prev_hunk<CR>";
+      key = "[h";
+      options = {
+        silent = true;
+        desc = "Previous git hunk";
       };
     }
   ];
