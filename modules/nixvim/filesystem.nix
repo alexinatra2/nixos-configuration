6ffe3e54cbfb9acb4e1941bf1b29feb0241af3dd
Alexander Holzknecht <alexander.holzknecht@3m5.de>
2026-02-02 12:33:33 +0100

    configure oil in nixvim

--- modules/nixvim/filesystem.nix
+++ modules/nixvim/filesystem.nix
@@ -1,5 +1,17 @@
 {
-  plugins.oil.enable = true;
+  plugins.oil = {
+    enable = true;
+    settings = {
+      columns = [
+        "icon"
+        "permissions"
+        "size"
+      ];
+      delete_to_trash = true;
+      skip_confirm_for_simple_edits = true;
+    };
+  };
+
   plugins.neo-tree = {
     enable = true;
     settings = {
@@ -9,24 +21,22 @@
           leave_dirs_open = false;
         };
       };
-      window = {
-        auto_focus = false;
-      };
     };
   };
 
   keymaps = [
     {
-      key = "<leader>e";
-      action = "<cmd>Oil<CR>";
+      key = "<A-w>";
+      action = "<CMD>lua require('oil').toggle_float()<CR>";
       options = {
+        desc = "Toggle Oil float";
         silent = true;
       };
     }
 
     {
       key = "<A-e>";
-      action = "<CMD>Neotree filesystem reveal toggle<CR>";
+      action = "<CMD>Neotree filesystem reveal toggle action=show<CR>";
       options = {
         desc = "Toggle Neo‑Tree (reveal current file)";
         silent = true;
