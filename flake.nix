2937b3141c77ea8726e38f00b524223f86008582
alexinatra2 <a.holzknecht@gmx.de>
2026-02-06 15:02:30 +0100

    add firefox-addons

--- flake.nix
+++ flake.nix
@@ -23,6 +23,11 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -81,7 +86,6 @@
         (inputs.nvf.lib.neovimConfiguration {
           inherit pkgs;
           modules = [ ./modules/nvf ];
-       	})
-	.neovim;
-  };
+        }).neovim;
+    };
 }
