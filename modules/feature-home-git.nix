17619030aa06a3ac5affc0923bebffdc0aadaf42
alexinatra2 <a.holzknecht@gmx.de>
2026-02-18 23:22:30 +0100

    fix git not being enabled

--- modules/feature-home-git.nix
+++ modules/feature-home-git.nix
@@ -1,16 +1,19 @@
 {
   flake.modules.homeManager.git = {
-    programs.git.settings = {
-      user = {
-        name = "alexinatra2";
-        email = "a.holzknecht@gmx.de";
-      };
-      alias = {
-        s = "status";
-        c = "commit";
-        ca = "commit -a";
-        hist = "log --oneline --graph --decorate --all";
-        undo = "reset --soft HEAD~1";
+    programs.git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "alexinatra2";
+          email = "a.holzknecht@gmx.de";
+        };
+        alias = {
+          s = "status";
+          c = "commit";
+          ca = "commit -a";
+          hist = "log --oneline --graph --decorate --all";
+          undo = "reset --soft HEAD~1";
+        };
       };
     };
   };
