ae8f499624842065bf12a39f9ff6c70d099d417b
alexinatra2 <a.holzknecht@gmx.de>
2026-03-01 18:18:52 +0100

    change netrw to oil

--- modules/nixvim/filesystem.nix
+++ modules/nixvim/filesystem.nix
@@ -1,8 +1,20 @@
 {
+  plugins.oil = {
+    enable = true;
+
+    settings = {
+      default_file_explorer = true;
+      columns = [
+        "icon"
+        "size"
+      ];
+      skip_confirm_for_simple_edits = true;
+    };
+  };
   keymaps = [
     {
       key = "<M-e>";
-      action = "<CMD>Lex<CR>";
+      action = "<CMD>lua require('oil').open_float()<CR>";
       options = {
         silent = true;
       };
