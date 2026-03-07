c83d68abd95ef9ceaee763e2efe827ae806a60c4
alexinatra2 <a.holzknecht@gmx.de>
2026-03-07 12:20:46 +0100

    add stable nixpkgs along with unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Multi-platform system configurations (NixOS + Darwin)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
