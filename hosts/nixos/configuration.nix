95945bb935f2847ff0cc2ae52783e95049e27766
alexinatra2 <a.holzknecht@gmx.de>
2026-04-01 17:52:41 +0200

    add fonts

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -37,23 +37,6 @@
     efi.canTouchEfiVariables = true;
   };
 
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
-  };
-
   # Enable the X11 windowing system.
   services = {
     xserver = {
@@ -129,6 +112,16 @@
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
+    corefonts
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
   ];
 
   hardware = {
--- modules/linux/nixos-locale.nix
+++ modules/linux/nixos-locale.nix
@@ -20,7 +20,7 @@
       LC_NUMERIC = "de_DE.UTF-8";
       LC_PAPER = "de_DE.UTF-8";
       LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
   };
 
