bbada825b3b0e33715059d13248a96997017f969
alexinatra2 <a.holzknecht@gmx.de>
2026-04-19 16:32:38 +0200

    add sync-noctalia-settings output

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,18 +1,35 @@
 { self, inputs, ... }:
 {
   perSystem =
-    { system, ... }:
+    {
+      system,
+      lib,
+      ...
+    }:
     let
       pkgs = import inputs.nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
+      myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+        inherit pkgs;
+        settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+      };
+      syncNoctaliaSettings = pkgs.writeShellApplication {
+        name = "sync-noctalia-settings";
+        text = ''
+          ${lib.getExe myNoctalia} ipc call state all > ~/nixos-configuration/modules/features/noctalia.json
+        '';
+      };
     in
     if pkgs.stdenv.isLinux then
       {
-        packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-          inherit pkgs;
-          settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+        packages = {
+          inherit myNoctalia syncNoctaliaSettings;
+        };
+        apps.sync-noctalia-settings = {
+          type = "app";
+          program = "${syncNoctaliaSettings}/bin/sync-noctalia-settings";
         };
       }
     else
