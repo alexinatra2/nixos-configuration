3c540842a58b4ffbe34b5f95ff5c248b76505bb6
alexinatra2 <a.holzknecht@gmx.de>
2026-04-21 17:35:28 +0200

    update virtualization feature with extra specialisation

--- modules/features/virtualization.nix
+++ modules/features/virtualization.nix
@@ -11,18 +11,54 @@ in
     }:
     {
       programs.virt-manager.enable = true;
+
       users.groups.libvirtd.members = [ "${username}" ];
+
+      environment.systemPackages = with pkgs; [
+        quickemu
+        quickgui
+      ];
 
       virtualisation = {
         containers.enable = true;
-        podman.enable = true;
-        docker.enable = true;
-        libvirtd = {
-          qemu.swtpm.enable = true;
+
+        podman = {
           enable = true;
+          dockerCompat = true;
+          defaultNetwork.settings = {
+            dns_enabled = true;
+          };
+        };
+
+        docker.enable = false;
+
+        libvirtd = {
+          enable = true;
+          onBoot = "start";
+          onShutdown = "shutdown";
+          qemu = {
+            package = pkgs.qemu_kvm;
+            swtpm.enable = true;
+          };
         };
 
         spiceUSBRedirection.enable = true;
+      };
+
+      specialisation = {
+        native-docker.configuration = {
+          virtualisation = {
+            podman = {
+              enable = lib.mkForce false;
+              dockerCompat = lib.mkForce false;
+            };
+            docker.enable = lib.mkForce true;
+          };
+        };
+
+        full-emulation.configuration = {
+          virtualisation.libvirtd.qemu.package = lib.mkForce pkgs.qemu;
+        };
       };
 
       security.rtkit.enable = true;
