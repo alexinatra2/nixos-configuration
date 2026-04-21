d9e32c9f803bda606cbd527d67342fdfd4af74f5
alexinatra2 <a.holzknecht@gmx.de>
2026-04-21 13:28:51 +0200

    update secrets

--- modules/features/sops.nix
+++ modules/features/sops.nix
@@ -1,7 +1,10 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.sops =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
+    let
+      user = config.users.users.alexander.name;
+    in
     {
       imports = [ inputs.sops-nix.nixosModules.sops ];
 
@@ -9,10 +12,24 @@
         sops
         age
       ];
+
+      sops = {
+        defaultSopsFile = ../../secrets/secrets.yaml;
+        defaultSopsFormat = "yaml";
+        age.keyFile = "/home/alexander/.config/sops/age/keys.txt";
+
+	secrets = {
+	  example-key = { };
+
+	  "myservice/my_subdir/my_secret" = { 
+	    owner = user;
+	  };
+	};
+      };
     };
 
   flake.modules.homeManager.sops =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
     {
       imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
@@ -21,7 +38,8 @@
         age
       ];
 
-      sops = {
+      home.sessionVariables = {
+        SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
       };
     };
 }
