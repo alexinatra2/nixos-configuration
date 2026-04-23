78baaaaa1b950505dc64667a1772357f1292ff07
alexinatra2 <a.holzknecht@gmx.de>
2026-04-23 15:44:17 +0200

    add ssh config utilities to ssh feature to configure ssh config ips using secrets repo

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -1,19 +1,50 @@
 { self, inputs, ... }:
 {
-  flake.modules.homeManager.ssh = {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
+  flake.modules.homeManager.ssh =
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      homelabHosts = [
+        "proxmox"
+        "openclaw"
+      ];
 
-      matchBlocks = {
-        "public" = {
-          host = "gitlab.com github.com";
-          identitiesOnly = true;
-          identityFile = [
-            "~/.ssh/id_ed25519"
-          ];
+      mkHostTemplate = host: {
+        "ssh/matchblocks/${host}" = {
+          content = ''
+            Host ${host}
+              HostName ${config.sops.placeholder."homelab/${host}/ip"}
+              User ${config.sops.placeholder."homelab/${host}/user"}
+          '';
+          mode = "0600";
         };
       };
+
+      mkHostSecrets = host: {
+        "homelab/${host}/ip" = { };
+        "homelab/${host}/user" = { };
+      };
+    in
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+
+        matchBlocks = {
+          "*" = {
+            identitiesOnly = true;
+            identityFile = [ "~/.ssh/id_ed25519" ];
+          };
+        };
+
+        includes = map (host: config.sops.templates."ssh/matchblocks/${host}".path) homelabHosts;
+      };
+
+      sops.templates = lib.foldl' lib.recursiveUpdate { } (map mkHostTemplate homelabHosts);
+
+      sops.secrets = lib.foldl' lib.recursiveUpdate { } (map mkHostSecrets homelabHosts);
     };
-  };
 }
