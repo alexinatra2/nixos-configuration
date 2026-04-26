44cadfc9a5649847f9b191b839fe01f96496023f
alexinatra2 <a.holzknecht@gmx.de>
2026-04-26 17:56:38 +0200

    fix docker compat issue

--- modules/features/virtualization.nix
+++ modules/features/virtualization.nix
@@ -25,6 +25,7 @@ in
         podman = {
           enable = true;
           dockerCompat = true;
+          dockerSocket.enable = true;
           defaultNetwork.settings = {
             dns_enabled = true;
           };
