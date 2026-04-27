e2b6d0adfbee795f7882db76a1e30d429deb68fc
alexinatra2 <a.holzknecht@gmx.de>
2026-04-28 00:06:01 +0200

    make tailscale not require sudo

--- modules/features/tailscale.nix
+++ modules/features/tailscale.nix
@@ -1,7 +1,15 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.tailscale =
-    { config, ... }:
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      normalUsers = lib.attrNames (lib.filterAttrs (_: userCfg: userCfg.isNormalUser or false) config.users.users);
+      operatorUser = if normalUsers == [ ] then "root" else builtins.head normalUsers;
+    in
     {
       sops.secrets."tailscale/authkey" = {
         restartUnits = [ "tailscaled-autoconnect.service" ];
@@ -10,6 +18,7 @@
       services.tailscale = {
         enable = true;
         authKeyFile = config.sops.secrets."tailscale/authkey".path;
+        extraSetFlags = [ "--operator=${operatorUser}" ];
       };
     };
 }
