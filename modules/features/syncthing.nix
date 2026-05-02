562adac82d7aaf5a9261ee24382de6d85c72c8e4
alexinatra2 <a.holzknecht@gmx.de>
2026-05-02 22:29:08 +0200

    setup syncthing

--- modules/features/syncthing.nix
+++ modules/features/syncthing.nix
@@ -12,6 +12,9 @@
       services.syncthing = {
         enable = true;
         openDefaultPorts = true;
+        user = "alexander";
+        group = "users";
+        dataDir = "/home/alexander";
         guiPasswordFile = config.sops.secrets."syncthing/password".path;
         cert = config.sops.secrets."syncthing/cert".path;
         key = config.sops.secrets."syncthing/key".path;
@@ -22,6 +25,13 @@
             pixel7 = {
               id = "S6GEWYT-ST3KUYP-PBEE6WG-TYKMUL7-X6UQPX2-IQOL567-5YKKBQZ-VFO3WQV";
             };
+          };
+          folders.camera = {
+            id = "v283i-tw1dt";
+            label = "Camera";
+            path = "/home/alexander/Pictures/Pixel7";
+            type = "receiveonly";
+            devices = [ "pixel7" ];
           };
         };
       };
