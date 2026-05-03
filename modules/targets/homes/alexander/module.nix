fe4f420004f42fee19772b6fe3728f568b4f873a
alexinatra2 <a.holzknecht@gmx.de>
2026-05-04 01:21:40 +0200

    remove unused home-manager targets

--- modules/targets/homes/alexander/module.nix
+++ modules/targets/homes/alexander/module.nix
@@ -4,28 +4,27 @@ let
 in
 {
   flake.modules.homeManager.${profileName} = {
-    imports = [
-      self.modules.homeManager.base
-      self.modules.homeManager.firefox
-      self.modules.homeManager.fonts
-      self.modules.homeManager.generations
-      self.modules.homeManager.git
-      self.modules.homeManager.comfyui
-      self.modules.homeManager.mcp
-      self.modules.homeManager.neovim
-      self.modules.homeManager.obsidian
-      self.modules.homeManager.opencode
-      self.modules.homeManager.pdf
-      self.modules.homeManager.privatepackages
-      self.modules.homeManager."rust-dev"
-      self.modules.homeManager.shell
-      self.modules.homeManager.slide-creation
-      self.modules.homeManager.ssh
-      self.modules.homeManager.stylix
-      self.modules.homeManager.tmux
-      self.modules.homeManager.music-creation
-      self.modules.homeManager.llms
-      self.modules.homeManager.sops
+    imports = with self.modules.homeManager; [
+      base
+      firefox
+      fonts
+      generations
+      git
+      comfyui
+      mcp
+      neovim
+      obsidian
+      opencode
+      pdf
+      privatepackages
+      shell
+      slide-creation
+      ssh
+      stylix
+      tmux
+      music-creation
+      llms
+      sops
     ];
   };
 }
