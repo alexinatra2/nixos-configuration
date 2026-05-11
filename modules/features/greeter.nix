870c056533c9203ec3254020ddda248356a2a0a8
alexinatra2 <a.holzknecht@gmx.de>
2026-05-11 17:56:02 +0200

    update greeter to use plymouth

--- modules/features/greeter.nix
+++ modules/features/greeter.nix
@@ -3,9 +3,91 @@
   flake.nixosModules.greeter =
     {
       config,
+      lib,
+      pkgs,
       ...
     }:
+    let
+      inherit (config.lib.stylix.colors) base00 base03 base05 base08 base0A base0B;
+
+      themeName = "ly-colormix";
+      spinnerThemeDir = "${pkgs.plymouth}/share/plymouth/themes/spinner";
+
+      lyLogoSvg = pkgs.writeText "${themeName}.svg" ''
+        <svg xmlns="http://www.w3.org/2000/svg" width="640" height="200" viewBox="0 0 640 200">
+          <rect x="80" y="40" width="360" height="28" rx="14" fill="#${base08}" />
+          <rect x="140" y="86" width="360" height="28" rx="14" fill="#${base0B}" />
+          <rect x="200" y="132" width="360" height="28" rx="14" fill="#${base0A}" />
+        </svg>
+      '';
+
+      lyLogo = pkgs.runCommand "${themeName}-logo.png" { nativeBuildInputs = [ pkgs.librsvg ]; } ''
+        rsvg-convert --background-color=transparent --format=png --output "$out" "${lyLogoSvg}"
+      '';
+
+      plymouthThemeFile = pkgs.writeText "${themeName}.plymouth" ''
+        [Plymouth Theme]
+        Name=Ly Colormix
+        Description=Spinner theme coloured to match the ly greeter.
+        ModuleName=two-step
+
+        [two-step]
+        Font=Cantarell 12
+        TitleFont=Cantarell Light 30
+        ImageDir=${spinnerThemeDir}
+        DialogHorizontalAlignment=.5
+        DialogVerticalAlignment=.382
+        TitleHorizontalAlignment=.5
+        TitleVerticalAlignment=.382
+        HorizontalAlignment=.5
+        VerticalAlignment=.7
+        WatermarkHorizontalAlignment=.5
+        WatermarkVerticalAlignment=.46
+        Transition=none
+        TransitionDuration=0.0
+        BackgroundStartColor=0x${base00}
+        BackgroundEndColor=0x${base00}
+        ProgressBarBackgroundColor=0x${base03}
+        ProgressBarForegroundColor=0x${base0A}
+        ProgressBarBorderColor=0x${base05}
+        MessageBelowAnimation=true
+
+        [boot-up]
+        UseEndAnimation=false
+
+        [shutdown]
+        UseEndAnimation=false
+
+        [reboot]
+        UseEndAnimation=false
+      '';
+
+      plymouthThemePackage = pkgs.runCommand "${themeName}-theme" { } ''
+        theme_dir="$out/share/plymouth/themes/${themeName}"
+        mkdir -p "$theme_dir"
+        install -m0644 "${plymouthThemeFile}" "$theme_dir/${themeName}.plymouth"
+      '';
+    in
     {
+      boot = {
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        kernelParams = [
+          "quiet"
+          "splash"
+          "udev.log_level=3"
+          "rd.udev.log_level=3"
+          "vt.global_cursor_default=0"
+        ];
+
+        plymouth = {
+          enable = true;
+          theme = lib.mkForce themeName;
+          themePackages = [ plymouthThemePackage ];
+          logo = lyLogo;
+        };
+      };
+
       services.displayManager.sddm.enable = false;
       services.greetd.enable = false;
 
