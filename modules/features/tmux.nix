543ba50d762d2dab0cb0042863e36a265e02d505
alexinatra2 <a.holzknecht@gmx.de>
2026-05-28 15:52:37 +0200

    tmux: enable vi keys and sessionx defaults

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -5,16 +5,28 @@
     {
       programs.tmux = {
         enable = true;
+        baseIndex = 1;
         clock24 = true;
+        customPaneNavigationAndResize = true;
         historyLimit = 10000;
+        keyMode = "vi";
         plugins = with pkgs.tmuxPlugins; [
           sensible
           yank
           resurrect
           continuum
+          tmux-sessionx
         ];
         extraConfig = ''
           set -g mouse on
+          set -g status-keys vi
+          set -s extended-keys on
+          set -as terminal-features 'xterm*:extkeys'
+          set -as terminal-features 'kitty:extkeys'
+          setw -g pane-base-index 1
+          set -g @sessionx-fzf-builtin-tmux 'on'
+          set -g @sessionx-preview-location 'right'
+          set -g @sessionx-preview-ratio '55%'
         '';
       };
     };
@@ -24,7 +36,10 @@
     {
       programs.tmux = {
         enable = true;
+        baseIndex = 1;
         clock24 = true;
+        customPaneNavigationAndResize = true;
+        keyMode = "vi";
         mouse = true;
         historyLimit = 10000;
         plugins = with pkgs.tmuxPlugins; [
@@ -33,6 +48,13 @@
           resurrect
           continuum
         ];
+        extraConfig = ''
+          set -g status-keys vi
+          set -s extended-keys on
+          set -as terminal-features 'xterm*:extkeys'
+          set -as terminal-features 'kitty:extkeys'
+          setw -g pane-base-index 1
+        '';
       };
 
       home.packages = with pkgs; [
