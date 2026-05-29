8b1ade0fb49906c17468931c557a1c60aba9a45c
alexinatra2 <a.holzknecht@gmx.de>
2026-05-29 17:27:49 +0200

    fix secrets

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -51,7 +51,7 @@
           group = "nginx";
           mode = "0440";
           restartUnits = [ "nginx.service" ];
-          sopsFile = ../targets/hosts/warden/secrets.yaml;
+          sopsFile = ../../hosts/warden/secrets.yaml;
         };
 
         "vaultwarden/tls/key" = {
@@ -59,7 +59,7 @@
           group = "nginx";
           mode = "0440";
           restartUnits = [ "nginx.service" ];
-          sopsFile = ../targets/hosts/warden/secrets.yaml;
+          sopsFile = ../../hosts/warden/secrets.yaml;
         };
       };
 
