3af1a09c251422f2d81a80f005b851592a868115
alexinatra2 <a.holzknecht@gmx.de>
2026-06-01 15:46:14 +0200

    update thunderbird config

--- modules/features/thunderbird.nix
+++ modules/features/thunderbird.nix
@@ -9,14 +9,22 @@
     {
       imports = [ self.modules.homeManager.sops ];
 
-      sops.secrets."mail/thunderbird/woodservant/app-password" = { };
+      sops.secrets = {
+        "thunderbird/woodservant/app-password" = { };
+        "thunderbird/gmx/password" = { };
+        "thunderbird/gmail/password" = { };
+      };
 
       programs.thunderbird = {
         enable = true;
 
         profiles.default = {
           isDefault = true;
-          accountsOrder = [ "woodservant" ];
+          accountsOrder = [
+            "woodservant"
+            "gmx"
+            "gmail"
+          ];
         };
       };
 
@@ -29,7 +37,7 @@
 
           # Keep mailbox credentials out of the Nix store.
           passwordCommand = "${pkgs.coreutils}/bin/cat ${
-            config.sops.secrets."mail/thunderbird/woodservant/app-password".path
+            config.sops.secrets."thunderbird/woodservant/app-password".path
           }";
 
           imap = {
@@ -40,6 +48,62 @@
 
           smtp = {
             host = "smtp.purelymail.com";
+            port = 465;
+            tls.enable = true;
+          };
+
+          thunderbird = {
+            enable = true;
+            profiles = [ "default" ];
+          };
+        };
+
+        gmx = {
+          address = "a.holzknecht@gmx.de";
+          realName = config.local.base.fullName;
+          userName = "a.holzknecht@gmx.de";
+
+          # Keep mailbox credentials out of the Nix store.
+          passwordCommand = "${pkgs.coreutils}/bin/cat ${
+            config.sops.secrets."thunderbird/gmx/password".path
+          }";
+
+          imap = {
+            host = "imap.gmx.net";
+            port = 993;
+            tls.enable = true;
+          };
+
+          smtp = {
+            host = "mail.gmx.net";
+            port = 587;
+            tls.enable = true;
+          };
+
+          thunderbird = {
+            enable = true;
+            profiles = [ "default" ];
+          };
+        };
+
+        gmail = {
+          address = "alexander.holzknecht@gmail.com";
+          realName = config.local.base.fullName;
+          userName = "alexander.holzknecht@gmail.com";
+
+          # Keep mailbox credentials out of the Nix store.
+          passwordCommand = "${pkgs.coreutils}/bin/cat ${
+            config.sops.secrets."thunderbird/gmail/password".path
+          }";
+
+          imap = {
+            host = "imap.gmail.com";
+            port = 993;
+            tls.enable = true;
+          };
+
+          smtp = {
+            host = "smtp.gmail.com";
             port = 465;
             tls.enable = true;
           };
