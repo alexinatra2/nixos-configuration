827b5f11ebdb0600d48519847eead97d370100a3
alexinatra2 <a.holzknecht@gmx.de>
2026-06-08 12:06:59 +0200

    add tmux pane switching vim-style bindings

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -18,6 +18,8 @@
           tmux-sessionx
         ];
         extraConfig = ''
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+/)?g?(view|l?n?vim?x?)(diff)?$'"
+
           unbind C-b
           set -g prefix C-Space
           bind C-Space send-prefix
@@ -28,6 +30,10 @@
           set -as terminal-features 'xterm*:extkeys'
           set -as terminal-features 'kitty:extkeys'
           setw -g pane-base-index 1
+          bind -n M-h if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
+          bind -n M-j if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
+          bind -n M-k if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
+          bind -n M-l if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
           bind c new-window -c '#{pane_current_path}'
           bind '"' split-window -v -c '#{pane_current_path}'
           bind % split-window -h -c '#{pane_current_path}'
@@ -58,6 +64,8 @@
           continuum
         ];
         extraConfig = ''
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+/)?g?(view|l?n?vim?x?)(diff)?$'"
+
           unbind C-b
           set -g prefix C-Space
           bind C-Space send-prefix
@@ -67,6 +75,10 @@
           set -as terminal-features 'xterm*:extkeys'
           set -as terminal-features 'kitty:extkeys'
           setw -g pane-base-index 1
+          bind -n M-h if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
+          bind -n M-j if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
+          bind -n M-k if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
+          bind -n M-l if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
           bind c new-window -c '#{pane_current_path}'
           bind '"' split-window -v -c '#{pane_current_path}'
           bind % split-window -h -c '#{pane_current_path}'
