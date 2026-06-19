6dd7c55835115aa4dee060047164509f9f8201b2
alexinatra2 <a.holzknecht@gmx.de>
2026-06-19 11:10:49 +0200

    add useful tmux bindings

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -29,16 +29,25 @@
           set -s extended-keys always
           set -as terminal-features 'xterm*:extkeys'
           set -as terminal-features 'kitty:extkeys'
+
           setw -g pane-base-index 1
+
           bind -n M-h if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
           bind -n M-j if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
           bind -n M-k if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
           bind -n M-l if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
+
+          bind g new-window -n lazygit -c '#{pane_current_path}' '${pkgs.lazygit}/bin/lazygit'
+          bind r new-window -n serpl -c "#{pane_current_path}" "${pkgs.serpl}/bin/serpl"
+          bind o command-prompt -p "open app: " "new-window '%%'" 
+
           bind c new-window -c '#{pane_current_path}'
           bind '"' split-window -v -c '#{pane_current_path}'
           bind % split-window -h -c '#{pane_current_path}'
+
           bind x kill-pane
           bind X kill-window
+
           set -g @sessionx-fzf-builtin-tmux 'on'
           set -g @sessionx-preview-location 'right'
           set -g @sessionx-preview-ratio '55%'
