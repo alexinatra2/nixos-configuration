7750baa5240e0157d54b828f1620330c4badaa43
alexinatra2 <a.holzknecht@gmx.de>
2026-07-14 11:17:32 +0200

    refactor(tmux): simplify shortcuts and app windows

--- modules/features/tmux.nix
+++ modules/features/tmux.nix
@@ -17,8 +17,6 @@
           continuum
         ];
         extraConfig = ''
-          is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+/)?g?(view|l?n?vim?x?)(diff)?$'"
-
           unbind C-b
           set -g prefix C-Space
           bind C-Space send-prefix
@@ -31,14 +29,10 @@
 
           setw -g pane-base-index 1
 
-          bind -n M-h if-shell "$is_vim" 'send-keys M-h' 'select-pane -L'
-          bind -n M-j if-shell "$is_vim" 'send-keys M-j' 'select-pane -D'
-          bind -n M-k if-shell "$is_vim" 'send-keys M-k' 'select-pane -U'
-          bind -n M-l if-shell "$is_vim" 'send-keys M-l' 'select-pane -R'
-
           bind g new-window -n lazygit -c '#{pane_current_path}' '${pkgs.lazygit}/bin/lazygit'
           bind r new-window -n serpl -c "#{pane_current_path}" "${pkgs.serpl}/bin/serpl"
-          bind o command-prompt -p "open app: " "new-window '%%'" 
+          bind e new-window -n glow -c "#{pane_current_path}" "${pkgs.glow}/bin/glow"
+          bind o new-window -n opencode -c "#{pane_current_path}" "${pkgs.opencode}/bin/opencode"
 
           bind c new-window -c '#{pane_current_path}'
           bind '"' split-window -v -c '#{pane_current_path}'
