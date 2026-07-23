837ab01ecdba8cd95d5f20e99309de086f5de584
alexinatra2 <a.holzknecht@gmx.de>
2026-07-23 14:43:20 +0200

    feat(atlas): unlock data disk with tpm2

--- hosts/atlas/storage.nix
+++ hosts/atlas/storage.nix
@@ -2,6 +2,28 @@
 {
   imports = [ inputs.disko.nixosModules.disko ];
 
+  boot.initrd.systemd.enable = true;
+
+  boot.lanzaboote = {
+    configurationLimit = 8;
+    measuredBoot = {
+      enable = true;
+      pcrs = [
+        0
+        4
+        7
+      ];
+      autoCryptenroll = {
+        enable = true;
+        device = "/dev/disk/by-id/nvme-WD_Blue_SN570_1TB_231429805907-part1";
+      };
+    };
+  };
+
+  environment.etc."crypttab".text = ''
+    data /dev/disk/by-id/nvme-WD_Blue_SN570_1TB_231429805907-part1 - tpm2-device=auto
+  '';
+
   disko.devices.disk.data = {
     type = "disk";
     device = "/dev/disk/by-id/nvme-WD_Blue_SN570_1TB_231429805907";
@@ -13,7 +35,6 @@
         content = {
           type = "luks";
           name = "data";
-          initrdUnlock = true;
 
           content = {
             type = "btrfs";
@@ -22,9 +43,18 @@
               "data"
             ];
             subvolumes = {
-              "/vms".mountpoint = "/srv/vms";
-              "/shared".mountpoint = "/srv/shared";
-              "/backup-staging".mountpoint = "/srv/backup-staging";
+              "/vms" = {
+                mountpoint = "/srv/vms";
+                mountOptions = [ "x-systemd.requires=systemd-cryptsetup@data.service" ];
+              };
+              "/shared" = {
+                mountpoint = "/srv/shared";
+                mountOptions = [ "x-systemd.requires=systemd-cryptsetup@data.service" ];
+              };
+              "/backup-staging" = {
+                mountpoint = "/srv/backup-staging";
+                mountOptions = [ "x-systemd.requires=systemd-cryptsetup@data.service" ];
+              };
             };
           };
         };
